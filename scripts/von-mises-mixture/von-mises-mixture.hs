990f71dabf706daa5577ad3ec8a0850cda2c86da
Sacha Sokoloski <sacha.sokoloski@mailbox.org>
2021-08-19 15:29:05 +0200

    Finalized von-mises-mixture for tutorial

--- scripts/von-mises-mixture/von-mises-mixture.hs
+++ scripts/von-mises-mixture/von-mises-mixture.hs
@@ -19,12 +19,12 @@ import qualified Goal.Core.Vector.Storable as S
 -- VonMises Components --
 
 vm1,vm2,vm3 :: Source # (VonMises,VonMises)
-vm1 = fromTuple (1.5, 2, 4.5, 2)
-vm2 = fromTuple (3, 2, 3, 2)
-vm3 = fromTuple (4.5, 2, 1.5, 2)
+vm1 = fromTuple (1, 1.5, 4.5, 2)
+vm2 = fromTuple (3, 2, 3.5, 3)
+vm3 = fromTuple (4.5, 4, 1.5, 2)
 
 wghts :: Source # Categorical 2
-wghts = fromTuple (0.25,0.25)
+wghts = fromTuple (0.33,0.33)
 
 strumxmdl :: Source # Mixture (VonMises,VonMises) 2
 strumxmdl = joinSourceMixture (S.fromTuple (vm1,vm2,vm3)) wghts
@@ -36,30 +36,10 @@ trumxmdl = transition strumxmdl
 mxmdlint :: Source # Normal
 mxmdlint = fromTuple (0,0.1)
 
--- Mixture Distributions --
-
---vm1',vm2',vm3' :: Source # (VonMises,VonMises)
---vm1' = fromTuple (2, 0.5, pi, 0.5)
---vm2' = fromTuple (3, 0.5, pi, 0.5)
---vm3' = fromTuple (4, 0.5, pi, 0.5)
---
---vms' :: S.Vector 3 (Natural # (VonMises,VonMises))
---vms' = S.fromTuple (toNatural vm1',toNatural vm2',toNatural vm3')
---
---mix1',mix2' :: Double
---mix1' = 0.33
---mix2' = 0.33
---
---wghts' :: Source # Categorical 2
---wghts' = fromTuple (mix1',mix2')
---
---hrm0 :: Natural # Mixture (VonMises,VonMises) 2
---hrm0 = joinNaturalMixture vms' $ toNatural wghts'
-
 -- Training --
 
 nsmps :: Int
-nsmps = 50
+nsmps = 100
 
 eps :: Double
 eps = 0.05
@@ -114,25 +94,23 @@ main = do
     let xys = fst <$> cxys
 
     let emmxmdls = take nepchs $ iterate (vonMisesEM xys) mxmdl0
-        --itrmxmdl1 :: Natural # Mixture VonMises 2
-        --(itrmxmdl1,itrlls) = iterativeMixtureOptimization
-        --    (div nepchs 3) eps defaultAdamPursuit Nothing 0.1 xys $ toNatural vm2'
 
     let admmxmdls = take nepchs . takeEvery admmlt
             $ vanillaGradientSequence (logLikelihoodDifferential xys) eps defaultAdamPursuit mxmdl0
 
-    let trunlls = repeat . average $ negate . logObservableDensity trumxmdl <$> xys
-        emnlls = [ average $ negate . logObservableDensity mxmdl <$> xys | mxmdl <- emmxmdls ]
-        admnlls = [ average $ negate . logObservableDensity mxmdl <$> xys | mxmdl <- admmxmdls ]
-        --itrnlls = negate <$> concat itrlls
+    let trunlls = repeat $ logLikelihood xys trumxmdl
+        emnlls = logLikelihood xys <$> emmxmdls
+        admnlls = logLikelihood xys <$> admmxmdls
 
     let cedcsvs = zip3 trunlls emnlls admnlls
 
     let emmxmdl1 = last emmxmdls
         admmxmdl1 = last admmxmdls
-        [trucnfs,emcnfs,admcnfs] = mixtureModelToConfidenceCSV <$> [trumxmdl,emmxmdl1,admmxmdl1]
+        [trucnfs,emcnfs,admcnfs] =
+            mixtureModelToConfidenceCSV <$> [trumxmdl,emmxmdl1,admmxmdl1]
 
-    let [trumn,emmn,admmn] = mixtureModelToMeanCSV <$> [trumxmdl,emmxmdl1,admmxmdl1]
+    let [trumn,emmn,admmn] =
+            mixtureModelToMeanCSV <$> [trumxmdl,emmxmdl1,admmxmdl1]
 
     let ldpth = "data"
 
@@ -150,5 +128,5 @@ main = do
 
     goalExport ldpth "samples" xys
 
-    runGnuplot ldpth "cross-entropy-descent"
+    runGnuplot ldpth "log-likelihood-ascent"
     runGnuplot ldpth "mixture-components"
