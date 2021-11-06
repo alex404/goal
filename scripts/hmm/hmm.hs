7569a500093aed2bec4b3cecf7874ffc9b650dd4
alex404 <alex404@mailbox.org>
2021-11-06 14:13:44 +0100

    Tinkering with hmm.hs

--- scripts/hmm/hmm.hs
+++ scripts/hmm/hmm.hs
@@ -16,29 +16,56 @@ import Goal.Graphical
 import qualified Goal.Core.Vector.Storable as S
 
 
+--- Globals ---
+
+
+-- Simulation --
+
+pthln :: Int
+pthln = 8
+
+nsmps :: Int
+nsmps = 500
+
+nepchs :: Int
+nepchs = 500
+
 -- True Model --
 
 trns :: Natural # Affine Tensor (Categorical 2) (Categorical 2) (Categorical 2)
 trns = fst . split . toNatural $ joinMeanMixture
     ( S.fromTuple
-        ( fromTuple (0.1,0.2)
-        , fromTuple (0.7,0.2)
-        , fromTuple (0.2,0.7) )
+        ( fromTuple (0.2,0.2)
+        , fromTuple (0.6,0.2)
+        , fromTuple (0.2,0.6) )
     )  (fromTuple (0.33,0.33))
 
 emsn :: Natural # Affine Tensor (Categorical 2) (Categorical 2) (Categorical 2)
 emsn = fst . split . toNatural $ joinMeanMixture
     ( S.fromTuple
-        ( fromTuple (0.1,0.3)
-        , fromTuple (0.6,0.1)
-        , fromTuple (0.1,0.6) )
+        ( fromTuple (0.2,0.2)
+        , fromTuple (0.6,0.2)
+        , fromTuple (0.2,0.6) )
     )  (fromTuple (0.33,0.33))
 
 prr :: Natural # Categorical 2
-prr = toNatural (fromTuple (0.33,0.33) :: Mean # Categorical 2)
+prr = toNatural (fromTuple (0.2,0.35) :: Mean # Categorical 2)
 
 hmm :: Natural # HiddenMarkovModel 2 2
 hmm = joinLatentProcess prr emsn trns
+
+-- Testing
+
+bruteForceMarginalization :: [Int] -> (Int, Int) -> Double
+bruteForceMarginalization zs (stp,x0) =
+    let dnm = logSumExp $ logDensity hmm . zip zs <$> replicateM pthln xspc
+        nmrsqs = do
+            hds <- replicateM stp xspc
+            tls <- replicateM (pthln - stp - 1) xspc
+            return $ hds ++ [x0] ++ tls
+        nmr = logSumExp $ logDensity hmm . zip zs <$> nmrsqs
+     in exp $ nmr-dnm
+
 
 -- Learning
 
@@ -64,22 +91,40 @@ xspc = [0,1,2]
 main :: IO ()
 main = do
 
+
+    zss <- realize . replicateM nsmps $ map fst <$> sampleLatentProcess pthln hmm
+    let zs = head zss
+
+    putStrLn "\n-- Observable Densities --"
+
+    putStrLn "\nAnalytic:"
+    let smths = conjugatedSmoothing hmm zs
+    mapM_ print $ S.toList . categoricalWeights . toMean <$> smths
+
+    putStrLn "\nBrute Force:"
+    mapM_ print [ [ bruteForceMarginalization zs (stp,x) | x <- [0,1,2]] | stp <- [0..pthln-1]]
+
+    putStrLn "\n-- Learning --\n"
+
     trns0 :: Natural # Affine Tensor (Categorical 2) (Categorical 2) (Categorical 2)
-        <- realize $ uniformInitialize (-1,1)
+        <- realize $ uniformInitialize (-0.1,0.1)
     emsn0 :: Natural # Affine Tensor (Categorical 2) (Categorical 2) (Categorical 2)
-        <- realize $ uniformInitialize (-1,1)
-    prr0 :: Natural # Categorical 2 <- realize $ uniformInitialize (-1,1)
+        <- realize $ uniformInitialize (-0.1,0.1)
+    prr0 :: Natural # Categorical 2 <- realize $ uniformInitialize (-0.1,0.1)
 
     let hmm0 = joinLatentProcess prr0 emsn0 trns0
 
-    zss <- realize . replicateM 200 $ map fst <$> sampleLatentProcess 200 hmm
-
     let em = latentProcessExpectationMaximization zss
 
-        hmms = take 50 $ iterate em hmm0
+        hmms = take nepchs $ iterate em hmm0
 
-    let lls hmm' = average $ logObservableDensities hmm' zss
+    let lls hmm' =
+            let ll = average $ logObservableDensities hmm' zss
+                (prr',_,_) = splitLatentProcess hmm'
+            in (ll,show $ toMean prr')
 
+    putStrLn $ "\nTrue LL:" ++ show (fst $ lls hmm)
+    putStrLn "Gradient Ascent:"
     mapM_ (print . lls) hmms
 
     putStrLn "\nModels:"
@@ -93,16 +138,6 @@ main = do
 
 --- Graveyard ---
 
-
---bruteForceMarginalization :: Int -> [Int] -> (Int, Int) -> Double
---bruteForceMarginalization ln zs (stp,x0) =
---    let dnm = logSumExp $ logDensity hmm . zip zs <$> replicateM ln xspc
---        nmrsqs = do
---            hds <- replicateM stp xspc
---            tls <- replicateM (ln - stp - 1) xspc
---            return $ hds ++ [x0] ++ tls
---        nmr = logSumExp $ logDensity hmm . zip zs <$> nmrsqs
---     in exp $ nmr-dnm
 
     --zs <- realize $ map snd <$> sampleStateSpaceModel trns emsn 2 prr
 
@@ -137,9 +172,6 @@ main = do
     --let smths = fst $ conjugatedSmoothing trns emsn prr zs
     --putStrLn "\nSmoothing Probabilities:"
     --mapM_ print $ categoricalWeights <$> smths
-
-    --putStrLn "\nBrute Force:"
-    --mapM_ print [ [ bruteForceMarginalization ln zs (stp,x) | x <- [0,1,2]] | stp <- [0..ln-1]]
 
 
     --putStrLn "HMM Simulation:"
